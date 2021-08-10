047840b4bd6e5cc5b4c55b3860c63fc5028c89ef
chrisxesq <31022027+chrisxesq@users.noreply.github.com>
2021-08-10 16:37:43 -0400

    Create reflectingBear.py

---
+++ reflectingBear.py
@@ -0,0 +1,82 @@
+##Mathematics for Machine Learning: Linear Algebra week4
+# PACKAGE
+# Run this cell first once to load the dependancies.
+import numpy as np
+from numpy.linalg import norm, inv
+from numpy import transpose
+
+#copied from the previous assignment
+def gsBasis(A) :
+    
+    B = np.array(A, dtype=np.float_) # Make B as a copy of A, since we're going to alter it's values.
+    # Loop over all vectors, starting with zero, label them with i
+    for i in range(B.shape[1]) :
+        
+        # Inside that loop, loop over all previous vectors, j, to subtract.
+        for j in range(i) :
+            # Complete the code to subtract the overlap with previous vectors.
+            # you'll need the current vector B[:, i] and a previous vector B[:, j]
+            B[:, i] = B[:, i] - B[:, i] @ B[:, j] * B[:, j]
+        # Next insert code to do the normalisation test for B[:, i]
+        if la.norm(B[:, i]) > verySmallNumber :
+            B[:, i] = B[:, i] / la.norm(B[:, i])
+        else :
+            B[:, i] = np.zeros_like(B[:, i])
+            
+        
+            
+    # Finally, we return the result:
+    return B
+ 
+# GRADED FUNCTION
+# You should edit this cell.
+
+# In this function, you will return the transformation matrix T,
+# having built it out of an orthonormal basis set E that you create from Bear's Basis
+# and a transformation matrix in the mirror's coordinates TE.
+def build_reflection_matrix(bearBasis) : # The parameter bearBasis is a 2×2 matrix that is passed to the function.
+    # Use the gsBasis function on bearBasis to get the mirror's orthonormal basis.
+    E = gsBasis(bearBasis)
+    # Write a matrix in component form that performs the mirror's reflection in the mirror's basis.
+    # Recall, the mirror operates by negating the last component of a vector.
+    # Replace a,b,c,d with appropriate values
+    TE = np.array([[1, 0],
+                   [0, -1]])
+    # Combine the matrices E and TE to produce your transformation matrix.
+    T = E@TE@inv(E)
+    # Finally, we return the result. There is no need to change this line.
+    return T
+  
+#potting
+# First load Pyplot, a graph plotting library.
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+# This is the matrix of Bear's basis vectors.
+# (When you've done the exercise once, see what happns when you change Bear's basis.)
+bearBasis = np.array(
+    [[1,   -1],
+     [1.5, 2]])
+# This line uses your code to build a transformation matrix for us to use.
+T = build_reflection_matrix(bearBasis)
+
+# Bear is drawn as a set of polygons, the vertices of which are placed as a matrix list of column vectors.
+# We have three of these non-square matrix lists: bear_white_fur, bear_black_fur, and bear_face.
+# We'll make new lists of vertices by applying the T matrix you've calculated.
+reflected_bear_white_fur = T @ bear_white_fur
+reflected_bear_black_fur = T @ bear_black_fur
+reflected_bear_face = T @ bear_face
+
+# This next line runs a code to set up the graphics environment.
+ax = draw_mirror(bearBasis)
+
+# We'll first plot Bear, his white fur, his black fur, and his face.
+ax.fill(bear_white_fur[0], bear_white_fur[1], color=bear_white, zorder=1)
+ax.fill(bear_black_fur[0], bear_black_fur[1], color=bear_black, zorder=2)
+ax.plot(bear_face[0], bear_face[1], color=bear_white, zorder=3)
+
+# Next we'll plot Bear's reflection.
+ax.fill(reflected_bear_white_fur[0], reflected_bear_white_fur[1], color=bear_white, zorder=1)
+ax.fill(reflected_bear_black_fur[0], reflected_bear_black_fur[1], color=bear_black, zorder=2)
+ax.plot(reflected_bear_face[0], reflected_bear_face[1], color=bear_white, zorder=3);
+
